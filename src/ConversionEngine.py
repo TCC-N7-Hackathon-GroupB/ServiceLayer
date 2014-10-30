74edfe36e1f8c12709b2ac5b44ec509edcfeaa46
Paul Winters <paul.j.winters@gmail.com>
2014-10-30 16:37:20 -0500

    add mm to inch

--- src/ConversionEngine.py
+++ src/ConversionEngine.py
@@ -7,6 +7,13 @@ import re
 
 ### Unit converstion
 
+def mm_to_inches_unit(value):
+	# create a unit'ed object from the raw value
+	mm_value = value * ureg.millimeter
+
+	# perform the conversion and return
+	return mm_value.to(ureg.inch).magnitude
+
 def gram_meter_squared_to_lbs_acre_unit(value):
 	# create a unit'ed object from the raw value
 	g_m2_value = value * ureg('gram/meter**2')
@@ -15,6 +22,19 @@ def gram_meter_squared_to_lbs_acre_unit(value):
 	return g_m2_value.to('pound/acre').magnitude
 
 ### Data Conversion functions
+
+def mm_to_inch(json_data):
+	"""
+	"""
+	for variable in json_data.keys():
+		if re.match(".*-mm", variable):
+			# All keys that indicate variables in mm to be converted to inches
+			sections = ['lower', 'mean', 'median', 'upper']
+			for section in sections:
+				mm_values = json_data[variable][section]
+				inches_values = [ mm_to_inches_unit(mm_value) for mm_value in mm_values]
+				json_data[variable][section] = inches_values
+
 
 def gram_meter_squared_to_lbs_acre(json_data):
 	"""
@@ -29,7 +49,7 @@ def gram_meter_squared_to_lbs_acre(json_data):
 				json_data[variable][section] = lbs_acre_values
 
 
-_conversions = [gram_meter_squared_to_lbs_acre]
+_conversions = [gram_meter_squared_to_lbs_acre, mm_to_inch]
 
 def convert(json_data):
 	"""
